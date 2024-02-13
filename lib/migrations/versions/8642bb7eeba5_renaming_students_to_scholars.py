a46ef9f6a33ff58ba84112db277d330c195a561e
abdalla-wasaa <abdallawasaa7@gmail.com>
2024-02-13 15:03:30 +0300

    Attempted

---
+++ lib/migrations/versions/8642bb7eeba5_renaming_students_to_scholars.py
@@ -0,0 +1,24 @@
+"""Renaming students to scholars
+
+Revision ID: 8642bb7eeba5
+Revises: 791279dd0760
+Create Date: 2024-02-13 14:57:16.619856
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '8642bb7eeba5'
+down_revision = '791279dd0760'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
